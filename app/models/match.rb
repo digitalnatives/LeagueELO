694726e92c110976e3609f4b35c94e7d640c2622
Drahos Tamás <drahostamas@gmail.com>
2014-12-12 17:00:59 +0100

    Add Team players associations

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,9 @@
 class Match < ActiveRecord::Base
   has_many :match_players
   has_many :players, through: :match_players
+
+  has_many :team_a_match_players, -> { where(team: 'A') }, class_name: 'MatchPlayer'
+  has_many :team_b_match_players, -> { where(team: 'B') }, class_name: 'MatchPlayer'
+  has_many :team_a_players, through: :team_a_match_players, class_name: 'Player'
+  has_many :team_b_players, through: :team_b_match_players, class_name: 'Player'
 end
